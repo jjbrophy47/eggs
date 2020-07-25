9e1b2f225eff80db377e65978fc8e08a699c3c1f
jbrophy <jonathanbrophy47@gmail.com>
2020-07-24 21:40:18 -0700

    added results notebook

--- jobs/preprocess/batch.sh
+++ jobs/preprocess/batch.sh
@@ -1,3 +1,3 @@
-./jobs/preprocess/single.sh youtube 10000 2 1440 short
-./jobs/preprocess/single.sh twitter 10000 2 1440 short
-./jobs/preprocess/single.sh soundcloud 10000 2 1440 short
+./jobs/preprocess/single.sh youtube 100000000 10 1440 short
+./jobs/preprocess/single.sh twitter 100000000 10 1440 short
+./jobs/preprocess/single.sh soundcloud 100000000 10 1440 short
