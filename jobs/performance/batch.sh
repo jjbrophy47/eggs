3b48aec039f8dc67308acfd73c16efa5e034a4fe
jbrophy <jonathanbrophy47@gmail.com>
2020-07-30 16:21:28 -0700

    small update

--- jobs/performance/batch.sh
+++ jobs/performance/batch.sh
@@ -1,3 +1,3 @@
-./jobs/peformance/single.sh youtube 2 1440 short
-./jobs/peformance/single.sh twitter 3 1440 short
-./jobs/peformance/single.sh soundcloud 5 1440 short
+./jobs/performance/single.sh youtube 2 1440 short
+./jobs/performance/single.sh twitter 3 1440 short
+./jobs/performance/single.sh soundcloud 5 1440 short
