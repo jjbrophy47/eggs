af1e864c209371f6587aac0b3067d4a36d91c1d1
jbrophy <jonathanbrophy47@gmail.com>
2020-07-31 09:37:25 -0700

    small update

--- jobs/components/batch.sh
+++ jobs/components/batch.sh
@@ -2,22 +2,22 @@ sbatch --cpus-per-task=3 \
        --time=1440 \
        --partition=short \
        --job-name=AC_youtube \
-       --output=jobs/logs/components/$dataset \
-       --error=jobs/errors/components/$dataset \
+       --output=jobs/logs/components/youtube \
+       --error=jobs/errors/components/youtube \
        jobs/components/single.sh youtube
 
 sbatch --cpus-per-task=3 \
        --time=1440 \
        --partition=short \
        --job-name=AC_twitter \
-       --output=jobs/logs/components/$dataset \
-       --error=jobs/errors/components/$dataset \
+       --output=jobs/logs/components/twitter \
+       --error=jobs/errors/components/twitter \
        jobs/components/single.sh twitter
 
 sbatch --cpus-per-task=15 \
        --time=1440 \
        --partition=short \
        --job-name=AC_soundcloud \
-       --output=jobs/logs/components/$dataset \
-       --error=jobs/errors/components/$dataset \
+       --output=jobs/logs/components/soundcloud \
+       --error=jobs/errors/components/soundcloud \
        jobs/components/single.sh soundcloud
