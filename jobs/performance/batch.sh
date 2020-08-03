20c5b62dd7ca4b290f056a3343ad824a5aa70963
jbrophy <jonathanbrophy47@gmail.com>
2020-08-02 21:48:13 -0700

    small updates

--- jobs/performance/batch.sh
+++ jobs/performance/batch.sh
@@ -1,7 +1,7 @@
 # ./jobs/performance/sgl.sh youtube 2 1440 short
 # ./jobs/performance/sgl.sh twitter 3 1440 short
-# ./jobs/performance/sgl.sh soundcloud 5 1440 short
+./jobs/performance/sgl.sh soundcloud 5 1440 short
 
 # ./jobs/performance/mrf.sh youtube auc 2 1440 short
-./jobs/performance/mrf.sh twitter auc 3 4320 long auc
-# ./jobs/performance/mrf.sh soundcloud ap 5 10080 long
+# ./jobs/performance/mrf.sh twitter auc 3 4320 long
+./jobs/performance/mrf.sh soundcloud ap 6 10080 long
--- scripts/preprocess/features.py
+++ scripts/preprocess/features.py
@@ -129,8 +129,6 @@ def _ngrams(df, count_vectorizer=None, max_features=10000, ngram_range=(3, 3),
     """
     Generate ngram features based on the text.
     """
-
-    df['text'] = df['text'].fillna('')
     str_list = df['text'].tolist()
 
     if count_vectorizer is None:
