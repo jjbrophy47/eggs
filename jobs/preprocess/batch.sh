ad7c398aa4e5689af547c1e95e2688e58dcb4c89
jbrophy <jonathanbrophy47@gmail.com>
2020-08-10 15:47:06 -0700

    small updates

--- jobs/preprocess/batch.sh
+++ jobs/preprocess/batch.sh
@@ -1,3 +1,3 @@
-./jobs/preprocess/single.sh youtube 3 100000 1440 short
-./jobs/preprocess/single.sh twitter 5 100000 1440 short
-./jobs/preprocess/single.sh soundcloud 10 100000 1440 short
+./jobs/preprocess/single.sh youtube 100000 3 1440 short
+./jobs/preprocess/single.sh twitter 100000 5 1440 short
+./jobs/preprocess/single.sh soundcloud 100000 10 1440 short
