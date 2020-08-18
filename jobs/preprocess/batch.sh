29a140e8c843150e31aa4854bfd319025c766fce
jbrophy <jonathanbrophy47@gmail.com>
2020-08-18 12:52:02 -0700

    small updates

--- jobs/preprocess/batch.sh
+++ jobs/preprocess/batch.sh
@@ -1,3 +1,3 @@
-./jobs/preprocess/single.sh youtube 100000 3 1440 short
-./jobs/preprocess/single.sh twitter 100000 5 1440 short
-./jobs/preprocess/single.sh soundcloud 100000 10 1440 short
+./jobs/preprocess/single.sh youtube 200000 3 1440 short
+./jobs/preprocess/single.sh twitter 200000 5 1440 short
+./jobs/preprocess/single.sh soundcloud 200000 10 1440 short
--- jobs/preprocess/features.sh
+++ jobs/preprocess/features.sh
@@ -6,23 +6,20 @@ module load python3/3.7.5
 
 dataset=$1
 n_fold_samples=$2
-# nrows=$2
-# val_frac=$3
-# test_frac=$4
+val_frac=$3
+test_frac=$4
 
 python3 scripts/preprocess/features.py \
   --dataset $dataset \
   --feature_type 'limited' \
   --relations \
-  --n_fold_samples $n_fold_samples
-  # --nrows $nrows \
-  # --val_frac $val_frac \
-  # --test_frac $test_frac
+  --n_fold_samples $n_fold_samples \
+  --val_frac $val_frac \
+  --test_frac $test_frac
 
 python3 scripts/preprocess/features.py \
   --dataset $dataset \
   --feature_type 'full' \
-  --n_fold_samples $n_fold_samples
-  # --nrows $nrows \
-  # --val_frac $val_frac \
-  # --test_frac $test_frac
+  --n_fold_samples $n_fold_samples \
+  --val_frac $val_frac \
+  --test_frac $test_frac
